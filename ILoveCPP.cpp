837917b84b89583ee7e0509b760382afd24fd1a7
crystallin16 <62825997+crystallin16@users.noreply.github.com>
2021-02-05 18:59:39 -0500

    Add files via upload

---
+++ ILoveCPP.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Crystal Lin
+Course: CS 135 
+Instructor: 
+Assignment: Lab 1A
+
+codes i love C++ 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << “Enter a number: “;
+	int num;
+	cin >> num;
+
+	for (int i=0; i < num; i++)
+		cout << “I love C++” << endl;
+
+	return 0;
+}
